d23d8012274ed4c2e99c5c290aa72d8e7c0c5933
Alexander Merkulov <sasha@merqlove.ru>
2017-06-06 01:40:25 +0300

    Fix  sync_commit

--- postgres/scripts/nfs-config.sh
+++ postgres/scripts/nfs-config.sh
@@ -2,7 +2,7 @@
 set -e
 
 sed -i -e 's/#wal_log_hints = off/wal_log_hints = on/g' "$PGDATA/postgresql.conf"
-sed -i -e 's/#synchronous_commit = on/synchronous_commit = on/g' "$PGDATA/postgresql.conf"
+sed -i -e 's/#synchronous_commit = on/synchronous_commit = off/g' "$PGDATA/postgresql.conf"
 sed -i -e 's/synchronous_commit = on/synchronous_commit = off/g' "$PGDATA/postgresql.conf"
 cat "$PGDATA/postgresql.conf" | grep wal_log_hints
 cat "$PGDATA/postgresql.conf" | grep synchronous_commit
